0ec8c9352e997b5eeac3c565881569617d2d44a0
Rexford Essilfie <rexfordessilfie09@gmail.com>
2020-07-15 12:11:46 -0400

    Added file to store default timers

---
+++ utils/index.js
@@ -0,0 +1,22 @@
+export const DEFAULT_TIMERS = {
+    "laundry": {
+        name: "Laundry",
+        totalDuration: 19600,
+        timeLeft: 18000,
+        started: false
+    },
+
+    "food": {
+        name: "Food",
+        totalDuration: 19600,
+        timeLeft: 18000,
+        started: false
+    },
+
+    "pomodoro": {
+        name: "Pomodoro",
+        totalDuration: 19600,
+        timeLeft: 60,
+        started: true
+    }
+}
